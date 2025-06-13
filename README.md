ec2a9bb71a1f38cff3c95e75b1da22002ef9cc91
LeoDreamer <2200010825@stu.pku.edu.cn>
2025-06-13 21:41:25 +0800

    example for 2x2 and circulant

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Final Project for QASC
+
+This is the endterm assignment for Quantum Algorithms for Scientific Computation course in PKU 2025 Spring semester.
--- example.py
+++ example.py
@@ -3,9 +3,6 @@ from qiskit import QuantumCircuit
 from qiskit.circuit.library import MCXGate
 
 if __name__ == "__main__":
-    qc = QuantumCircuit(3, 3)
-    qc.h(0)
-    qc.cx(0, 1)
-    qc.cx(0, 2)
+    qc = QuantumCircuit(3,3)
     qc.measure([0, 1, 2], [0, 1, 2])
     qc.draw('latex_source', filename='example.tex')
